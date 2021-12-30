64648329d58e47faee5cbeb79f7c7971fe8f7559
Adeshina <adeshiname@gmail.com>
2021-12-30 05:18:38 -0800

    Add Questions to Exercises

--- Program.cs
+++ Program.cs
@@ -2,34 +2,46 @@
 namespace Intermidiate
 {
 
-    class Program{
+    class Program
+    {
 
-          static void Main(string[] args){
-              var post = new Post();
-              post.Title = "How to create a varibale";
-              post.Description = "Its on how to create a variable";
-              post.DateCreated = DateTime.Now;
+        // This file contains the usuage for both exercises i did.
+        // Thier respective files can be found on stopwatch.cs and post.cs and thier questions are also there 
+        // Created below are two methods for thier usage.
 
-             post.UpVote();
-             post.DownVote();
-             post.GetVotes();
+        static void Main(string[] args)
+        {
 
-
-
-            //   var stopwatch =new StopWatch();
-            //   try
-            //   {
-            //     stopwatch.Start();
-            //     stopwatch.Start();
-            //     stopwatch.Stop();
-            //     Console.WriteLine("StopWatch Duration is {0}", stopwatch.duration.Milliseconds);
-            //   }
-            //   catch (Exception)
-            //   {
-            //       Console.WriteLine("Timer Already Started");
-            //   }
-         
         }
-      
+
+        static void UsePost()
+        {
+            var post = new Post();
+            post.Title = "How to create a varibale";
+            post.Description = "Its on how to create a variable";
+            post.DateCreated = DateTime.Now;
+
+            post.UpVote();
+            post.DownVote();
+            post.GetVotes();
+        }
+
+        static void UseStopWatch()
+        {
+            var stopwatch = new StopWatch();
+            try
+            {
+                stopwatch.Start();
+                stopwatch.Start();
+                stopwatch.Stop();
+                Console.WriteLine("StopWatch Duration is {0}", stopwatch.duration.Milliseconds);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Timer Already Started");
+            }
+
+        }
+
     }
 }
